b14f9cd67210b67bab7928d38bb6545c13dd1f84
Valentin <rivettivalentin@gmail.com>
2021-07-09 19:38:45 -0300

    Added css loader, opmtimization and solved cannot get /url problem

--- dist/582.main.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkportfolio=self.webpackChunkportfolio||[]).push([[582],{582:(o,e,f)=>{"use strict";function t(o){}f.r(e),f.d(e,{default:()=>t}),f(294)}}]);
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
-const path = require('path')
+const
+path = require('path'),
+HtmlWebpackPlugin = require('html-webpack-plugin'),
+MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output:{
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
+    filename: 'index.js',
+    publicPath: '/',
+    clean: true
   },
   module:{
     rules: [
@@ -17,12 +22,28 @@ module.exports = {
             presets: ['@babel/preset-env', '@babel/preset-react']
           }
         }
+      },
+      {
+        test: /\.css/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin()
+  ],
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
     port: 9000,
+    historyApiFallback: true
   }
 }
