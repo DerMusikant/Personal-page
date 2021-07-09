914270910219b80ca2f93a4d463f15a3f248fcec
Valentin <rivettivalentin@gmail.com>
2021-07-08 22:00:05 -0300

    Header and footer basic info added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,41 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 
 export default class Footer extends React.Component{
   render(){
-    return (<h1>footer</h1>)
+    return (
+      <footer className='footer'>
+        <div className='footer__info'>
+          <div className='info__logo'>
+            To do: brand logo-motto
+          </div>
+          <div className='info__links'>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/about'>About</Link>
+              </li>
+              <li>
+                <Link to='/FAQ'>Frequent Questions</Link>
+              </li>
+              <li>
+                <Link to='/services'>Services</Link>
+              </li>
+              <li>
+                <Link to='/contact'>Contact Me</Link>
+              </li>
+            </ul>
+          </div>
+          <div className='info__contact'>
+            To do: contact info
+          </div>
+        </div>
+        <div className='footer__copyright'>
+          © Copyright Valentin Rivetti. All Rights Reserved
+        </div>
+      </footer>
+    )
   }
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,30 @@ import {Link} from 'react-router-dom'
 export default class Header extends React.Component{
   render(){
     return (
-      <>
-        <Link to='/'>Home</Link>
-        <Link to='/about'>About</Link>
-      </>
+      <header className='header'>
+        <div className='header__logo'>
+          <Link to='/'>To do: Logo</Link>
+        </div>
+        <div className='header__navbar'>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link>
+            </li>
+            <li>
+              <Link to='/FAQ'>Frequent Questions</Link>
+            </li>
+            <li>
+              <Link to='/services'>Services</Link>
+            </li>
+            <li>
+              <Link to='/contact'>Contact Me</Link>
+            </li>
+          </ul>
+        </div>
+      </header>
     )
   }
 }
