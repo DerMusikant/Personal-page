24fa58647674b74a83983f44d73d70175aecee76
DerMusikant <rivettivalentin@gmail.com>
2021-07-10 04:32:07 -0300

    Header responsive functionality completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,33 +4,31 @@ import {Link} from 'react-router-dom'
 export default class Header extends React.Component{
 
   handleClick = (e) => {
-    alert('')
+    document.getElementById('navBar').classList.toggle('show')
   }
 
   render(){
     return (
-      <header className='flex justify-between bg-red-600'>
+      <header className='relative flex flex-col align-center bg-red-600 md:flex-row md:justify-between'>
 
-        <div className='header__logo'>
-          <Link to='/'>To do: Logo</Link>
+        <div className='m-4'>
+          <Link to='/'>To do:<br /> Logo</Link>
         </div>
 
 
-        <div className='relative'>
-
-          <Link to='/' className='block mt-16 py-2'>Home</Link>
-          <Link to='/about' className='block py-2'>About</Link>
-          <Link to='/FAQ' className='block py-2'>Frequent Questions</Link>
-          <Link to='/services' className='block py-2'>Services</Link>
-          <Link to='/contact' className='block py-2'>Contact Me</Link>
-
-          <div className='w-10 absolute right-0 top-0 m-4' onClick={this.handleClick}>
+          <div className='items-center flex-col hidden md:flex md:flex-row' id='navBar'>
+            <Link to='/' className='p-3' onClick={this.handleClick}>Home</Link>
+            <Link to='/about' className='p-3' onClick={this.handleClick}>About</Link>
+            <Link to='/FAQ' className='p-3' onClick={this.handleClick}>Frequent Questions</Link>
+            <Link to='/services' className='p-3' onClick={this.handleClick}>Services</Link>
+            <Link to='/contact' className='p-3' onClick={this.handleClick}>Contact Me</Link>
+          </div>
+          <div className='w-10 absolute right-0 top-0 m-4 md:hidden' onClick={this.handleClick}>
             <div className='border-b-4 border-black mt-2'></div>
             <div className='border-b-4 border-black my-2'></div>
             <div className='border-b-4 border-black mb-2'></div>
           </div>
 
-        </div>
       </header>
     )
   }
