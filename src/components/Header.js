2910aa7631286a1d45af74006ebc8c278fdc6fc7
DerMusikant <rivettivalentin@gmail.com>
2021-07-10 01:19:52 -0300

    currently working on responsive navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,29 +9,27 @@ export default class Header extends React.Component{
 
   render(){
     return (
-      <header className='flex justify-between'>
+      <header className='flex justify-between bg-red-600'>
+
         <div className='header__logo'>
           <Link to='/'>To do: Logo</Link>
         </div>
-        <div className='h-4 w-8 bg-red-600' onClick={this.handleClick}></div>
-        <div className='header__navbar'>
-          <ul>
-            <li>
-              <Link to='/' className='hover:underline'>Home</Link>
-            </li>
-            <li>
-              <Link to='/about'>About</Link>
-            </li>
-            <li>
-              <Link to='/FAQ'>Frequent Questions</Link>
-            </li>
-            <li>
-              <Link to='/services'>Services</Link>
-            </li>
-            <li>
-              <Link to='/contact'>Contact Me</Link>
-            </li>
-          </ul>
+
+
+        <div className='relative'>
+
+          <Link to='/' className='block mt-16 py-2'>Home</Link>
+          <Link to='/about' className='block py-2'>About</Link>
+          <Link to='/FAQ' className='block py-2'>Frequent Questions</Link>
+          <Link to='/services' className='block py-2'>Services</Link>
+          <Link to='/contact' className='block py-2'>Contact Me</Link>
+
+          <div className='w-10 absolute right-0 top-0 m-4' onClick={this.handleClick}>
+            <div className='border-b-4 border-black mt-2'></div>
+            <div className='border-b-4 border-black my-2'></div>
+            <div className='border-b-4 border-black mb-2'></div>
+          </div>
+
         </div>
       </header>
     )
