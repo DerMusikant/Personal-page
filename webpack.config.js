565d12dcf9a407f695d2746913e2db1ce4aa8b37
DerMusikant <rivettivalentin@gmail.com>
2021-07-10 21:13:24 -0300

    problem solved?

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   output:{
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
-    publicPath: '/',
+    publicPath: '/dist/',
     clean: true
   },
   module:{
