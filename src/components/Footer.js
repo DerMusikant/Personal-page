e514bc4d9ab28ddf8949854f937213a355e8e6de
DerMusikant <rivettivalentin@gmail.com>
2021-07-11 22:07:54 -0300

    Footer looking pretty good

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,61 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
+import Logo from '../assets/icons/dragon-solid.svg'
+import Face from '../assets/icons/Facebook-square-brands.svg'
+import Linked from '../assets/icons/linkedin-brands.svg'
+import Git from '../assets/icons/github-brands.svg'
+
+
 export default class Footer extends React.Component{
   render(){
     return (
       <footer className='footer'>
-        <div className='footer__info'>
+        <div className='flex flex-col items-center justify-evenly bg-gradient-to-r from-red-600 to-red-main md:flex-row'>
           <div className='info__logo'>
-            To do: brand logo-motto
+            <div className='m-2'>
+              <Link to='/' className='contents'>
+                <div className='flex flex-col text-center items-center w-max plat md:flex-row'>
+                  <div className='m-2'>
+                  <Logo className='dragon plat' />
+                  <p>Draco</p>
+                  </div>
+                  <p>Your dream website<br />For your dream company</p>
+                </div>
+              </Link>
+            </div>
           </div>
-          <div className='info__links'>
-            <ul>
-              <li>
-                <Link to='/'>Home</Link>
-              </li>
-              <li>
-                <Link to='/about'>About</Link>
-              </li>
-              <li>
-                <Link to='/FAQ'>Frequent Questions</Link>
-              </li>
-              <li>
-                <Link to='/services'>Services</Link>
-              </li>
-              <li>
-                <Link to='/contact'>Contact Me</Link>
-              </li>
-            </ul>
+          <div className='flex flex-col items-center'>
+           <h3 className='font-bold'>Useful Links:</h3>
+            <div className='flex flex-col items-center md:flex-row'>
+                <Link to='/' className='useful m-2'>>Home</Link>
+                <Link to='/about' className='useful m-2'>>About</Link>
+                <Link to='/FAQ' className='useful m-2'>>Frequent Questions</Link>
+                <Link to='/services' className='useful m-2'>>Services</Link>
+                <Link to='/contact' className='useful m-2'>>Contact Me</Link>
+            </div>
           </div>
-          <div className='info__contact'>
-            To do: contact info
+          <div className='flex flex-col text-center'>
+            <p className='font-bold'>Follow me!</p>
+            <div className='flex'>
+              <a className='media' href='https://www.facebook.com/RivettiValentin/'>
+                <Face className='social-media p-2' />
+              </a>
+
+              <a className='media' href='https://www.linkedin.com/in/valentinrivetti/'>
+                <Linked className='social-media p-2' />
+              </a>
+
+              <a className='media' href='https://github.com/DerMusikant'>
+                <Git className='social-media p-2' />
+              </a>
+
+            </div>
+
           </div>
         </div>
-        <div className='footer__copyright'>
-          © Copyright Valentin Rivetti. All Rights Reserved
+        <div className='text-center'>
+          © Copyright Valentin Rivetti 2021. All Rights Reserved
         </div>
       </footer>
     )
