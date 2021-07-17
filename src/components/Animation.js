6dc8f2685f452a682c7fa942acc3407f2300b202
Valentin <rivettivalentin@gmail.com>
2021-07-17 16:37:34 -0300

    HOC not possible on react lazy. Used inherited class instead. TO DO: copy Anim class to Animation.js and export

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,6 +1,3 @@
-import React from 'react'
-import homeDragon from '../assets/img/red-dragon.png'
-
 class Anim extends React.Component {
 
     // INITIALIZE CLASS STATE
@@ -42,29 +39,3 @@ class Anim extends React.Component {
         })
       }
   }
-
-
-export default class Home extends Anim{
-
-  render(){
-    return (
-      <main className='flex flex-col justify-center items-center'>
-        { /* ----------------HOME INTRO START -------------- */ }
-        <section className='block-1 grid grid-rows-5 grid-cols-2 items-center text-center'>
-          <div className='col-span-2'>
-            <h3>A Dragon is inside your website</h3>
-          </div>
-          <div className='col-span-2'>
-            <p>I use the fire of the wise to help your company outstand on the internet realm.<br />
-            Together we can make the best website for your business.</p>
-          </div>
-          <div className='col-span-2'>
-            <div className='button anim'>Learn More</div>
-          </div>
-          <div className='row-start-1 row-span-2 col-span-2 flex justify-center anim' data-delay='1s'><img alt='' src={homeDragon} className='home-dragon'/></div>
-        </section>
-        { /* ----------------HOME INTRO END -------------- */ }
-      </main>
-    )
-  }
-}
