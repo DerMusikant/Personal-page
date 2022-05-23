6787ec4e0c21056991fbeab262b3677d803323ef
DerMusikant <rivettivalentin@gmail.com>
2022-05-23 14:36:22 -0300

    Finally added my photo to about page

--- src/components/about/PresentationCard.js
+++ src/components/about/PresentationCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Grid from '../general/Grid'
-import homeDragon from '../../assets/img/red-dragon.png'
+import valen from '../../assets/img/valen.jpg'
 
 
 export default class PresentationCard extends React.Component{
@@ -12,7 +12,7 @@ export default class PresentationCard extends React.Component{
     return (
         <>
             <Grid title='Frontend Developer and dedicated nerd 😋' description={<p>Hello there! The name's Valentin Rivetti, and I'm here to help you on your journey!
-              <br />I'm a Javascript Developer currently focusing on Frontend using ReactJs and training to be the best Fullstack!<br />Passionate about my work and hoping to make a great team...</p>} img={homeDragon} alt='Dragon logo' click={this.handleClick} button='See my work!'/>
+              <br />I'm a Javascript Developer currently focusing on Frontend using ReactJs and training to be the best Fullstack!<br />Passionate about my work and hoping to make a great team...</p>} img={valen} alt='Valentin Rivetti' click={this.handleClick} button='See my work!'/>
         </>
     )
   }
